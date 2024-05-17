a0a73fd21b2f8e0e8969aee81f5eee842691a1ee
amit <amit.dahiya.j@gmail.com>
2024-05-17 04:11:26 +0000

    microservice mysql code added

---
+++ microservice-mysql-demo/src/main/java/com/springboot/microservicemysqldemo/MicroserviceMysqlDemoApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.microservicemysqldemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceMysqlDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceMysqlDemoApplication.class, args);
+	}
+
+}
